e0edfee127885e87c1efc9ae0b1311bcb4a678f0
Pavel Kalugin <pkalugin48@gmail.com>
2021-10-20 18:35:47 +0500

    Add model of word and application context

---
+++ DataModel/Contexts/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using DataModel.Storages.Word.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel.Contexts
+{
+    /// <summary>
+    /// Класс контекста данных.
+    /// Содержит множество объектов, хранящееся в БД.
+    /// </summary>
+    public class ApplicationContext : DbContext
+    {
+        /// <summary>
+        /// Множество слов, таблица в БД, содержащяя само слово и количесвто его упоминаний.
+        /// </summary>
+        public DbSet<WordModel> Words { get; set; }
+
+        /// <summary>
+        /// Метод выполняет проверку, что БД создана, если нет, то создает ее.
+        /// </summary>
+        /// <param name="options"> Свойства контекста данных. </param>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
